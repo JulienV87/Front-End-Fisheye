0e28fd8968b985a722c1c1b28143a94581cee4a0
Julien Valleton <julien.valleton@gmail.com>
2023-03-03 20:29:57 +0100

    Récuperation des data Json avec fetch et affichage des portraits des photographes sur la page d'accueil

--- scripts/factories/photographer-factory.js
+++ scripts/factories/photographer-factory.js
@@ -3,15 +3,18 @@ function photographerFactory(data) {
 
     const picture = `assets/photographers/${portrait}`;
 
+
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute('alt', name)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
         return (article);
+
     }
     return { name, picture, getUserCardDOM }
 }
