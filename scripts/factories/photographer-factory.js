29ae064ac0b246aed79bc0042fadf8e5833f303e
Julien Valleton <julien.valleton@gmail.com>
2023-03-08 14:35:27 +0100

    Récuperation des data Json et affichage des data sur la page d'accueil

--- scripts/factories/photographer-factory.js
+++ scripts/factories/photographer-factory.js
@@ -1,20 +1,34 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+    function photographerFactory(data) {
+    const { id, name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
 
     function getUserCardDOM() {
+        const a = document.createElement('a')
+        a.setAttribute('href', `photographer.html?id=${id}`)
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         img.setAttribute('alt', name)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement('h3')
+        location.textContent = `${city}, ${country}`
+        const description = document.createElement('p')
+        description.textContent = tagline
+        const priceInfo = document.createElement('span')
+        priceInfo.textContent = `${price}€/jours`
+
+        a.appendChild(article)
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(location);
+        article.appendChild(description);
+        article.appendChild(priceInfo);
+
+        return a;
 
     }
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,17 +1,17 @@
     
         async function getPhotographers() {
-        
-        const urlApi = "./data/photographers.json";
-        const fetchJsonResult = await fetch(urlApi)
-        const data = await fetchJsonResult.json()
+
+        const url = "./data/photographers.json";
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data);
         
         const { photographers } = data
+        console.log(photographers);
         
-        return {
-          photographers,
-        }
+        return photographers
       }
-        
+    //--------------------------------------
         
     //     fetch(url, {
     //         method: "GET"
@@ -22,8 +22,9 @@
     //     });
     //     return fetchJsonResult;
     // }
+    //--------------------------------------
 
-    async function displayData(photographers) {
+    function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
@@ -34,7 +35,7 @@
     };
 
     async function init() {
-        const { photographers } = await getPhotographers();
+        const photographers = await getPhotographers();
         displayData(photographers);
     };
     
